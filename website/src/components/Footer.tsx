cad2cc5e9201a1b72b18e4ccbd4c6fdb84b389bc
ca5 <catinice@outlook.com>
2025-12-18 20:38:47 +0530

    fix footer coloring
    Signed-off-by: ca5 <catinice@outlook.com>
    
    Change-Id: Ief72f120115bbe926252581d4ab21fe76bdb93fe

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -54,9 +54,9 @@ const Footer: React.FC = () => {
       </footer>
 
       {/* Footer - positioned after all content */}
-      <div className="relative z-0 w-full h-64 bg-gradient-to-b from-card to-white flex justify-center items-center">
+      <div className="relative z-0 w-full h-64 bg-gradient-to-b from-card to-white dark:to-black flex justify-center items-center">
         <div className="relative overflow-hidden w-full h-full px-12">
-          <h2 className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/3 sm:text-[192px] text-[100px] md:text-[192px] text-black/10 font-bold">
+          <h2 className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/3 sm:text-[192px] text-[100px] md:text-[192px] text-black/10 dark:text-white/10 font-bold">
             LeetFeedback
           </h2>
         </div>
